c917735019cdfec95235863c649c184fc3e8aa37
MustNotBeGreacy <483858892@qq.com>
2023-02-20 20:24:35 +0800

    basic structure & ValidationLayers

---
+++ src/VulkanRenderer/Settings/config.h
@@ -0,0 +1,9 @@
+#pragma once 
+
+namespace config
+{
+	inline const uint16_t RESOLUTION_W = 800;
+	inline const uint16_t RESOLUTION_H = 600;
+
+	inline const char* TITLE = "Hello Triangle";
+}
---
+++ src/VulkanRenderer/main.cpp
@@ -0,0 +1,22 @@
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+
+#include "VulkanRenderer/HelloTriangleApp.h"
+
+int main()
+{
+    HelloTriangleApp app;
+
+    try
+    {
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 0;
+    }
+    return 0;
+}
